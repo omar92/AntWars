015f82fc8525e07d64741c12548bf9fe36e5d2af
AUEzzat <amr.m.ezzat@gmail.com>
2017-11-28 12:14:33 +0200

    dank2

--- js/scripts/constants.js
+++ js/scripts/constants.js
@@ -12,3 +12,24 @@ var LOCATIONS = {
     FOOD: 3,
     STONE: 4,
     STORE: 5
+};
+=======
+  STONE: 0,
+  WATER: 1,
+  WOOD: 2,
+  FOOD: 3
+};
+
+var RESOURCES_LOCATIONS = {
+  QUEEN: 0,
+  LAKE: 1,
+  WOOD: 2,
+  FOOD: 3
+};
+
+var LOCATION_TYPE = {
+  ELSE: 0,
+  RESOURCE: 2,
+  BUILDING: 2
+};
+>>>>>>> 9c902132c1f9c494e4ca2c7f6bbdbf93d79f6f4f
