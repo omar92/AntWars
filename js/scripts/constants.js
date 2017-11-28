e04e2f67c77efce2c1dce0f9d04af39a2953af39
ENG_OMARSLEAM\omar <omarabdelzaher@gmail.com>
2017-11-28 12:07:09 +0200

    add types for locations

--- js/scripts/constants.js
+++ js/scripts/constants.js
@@ -1,13 +1,19 @@
-var RESOURCES ={
-    STONE:0,
-    WATER:1,
-    WOOD:2,
-    FOOD:3
-};
-var RESOURCES_LOCATIONS ={
-    QUEEN:0,
-    LAKE:1,
-    WOOD:2,
-    FOOD:3
+var RESOURCES = {
+  STONE: 0,
+  WATER: 1,
+  WOOD: 2,
+  FOOD: 3
 };
 
+var RESOURCES_LOCATIONS = {
+  QUEEN: 0,
+  LAKE: 1,
+  WOOD: 2,
+  FOOD: 3
+};
+
+var LOCATION_TYPE = {
+  ELSE: 0,
+  RESOURCE: 2,
+  BUILDING: 2
+};
