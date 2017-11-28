9e927c119703248f4a9277f5cd3837fbf7338fca
AUEzzat <amr.m.ezzat@gmail.com>
2017-11-28 14:56:51 +0200

    fixed ant syntax errors

--- js/scripts/ant.js
+++ js/scripts/ant.js
@@ -1,29 +1,34 @@
 function Ant(antSprite) {
-    rightPath = {
+    currentPos = 0;
+    endPos = 0;
+    this.rightPath = {
         'x': [600, 480, 300, 300],
-        'leftY': [600, 460, 390, 0]
+        'y': [600, 460, 390, 0]
     };
-    leftPath = {
+    this.leftPath = {
         'x': [0, 120, 240, 240],
-        'rightY': [600, 450, 400, 0]
+        'y': [600, 450, 400, 0]
     }
     this.getResources = false;
     this.resource = null;
     this.path = [];
     this.alignLeft = true;
-    this.position = game.add.sprite(600, 600, 'test_ant.jpg');
+    this.position = game.add.sprite(600, 600, 'test_ant');
     this.moveNext = function() {
         if (this.path.length == 0) {
-            this.path.push(LOCATIONS.QUEEN)
+            endPos = LOCATIONS.QUEEN
         } else {
-            gotoPos(this.path.pop())
+            endPos = this.path.pop()
         }
     }
 
-    function update() {
-        this.position.y += 0.1
-        this.position.x += 0.1
-        this.position.y = Phaser.Math.linearInterpolation(rightPath.y, this.position.y);
-        this.position.x = Phaser.Math.linearInterpolation(rightPath.x, this.position.x);
+    this.update = function() {
+        currentPos += 0.1
+        if (currentPos < endPos) {
+            this.position.y = Phaser.Math.linearInterpolation(this.rightPath.y, currentPos);
+            this.position.x = Phaser.Math.linearInterpolation(this.rightPath.x, currentPos);
+        } else {
+            this.moveNext()
+        }
     }
 }
