7250da3a70b2bc16818a70e2f125b18ddbb2c770
ENG_OMARSLEAM\omar <omarabdelzaher@gmail.com>
2017-11-28 11:27:04 +0200

    no message

---
+++ js/scripts/constants.js
@@ -0,0 +1,7 @@
+var RESOURCES ={
+    STONE:0,
+    WATER:1,
+    WOOD:2,
+    FOOD:3
+};
+
---
+++ js/scripts/queen.js
@@ -0,0 +1,13 @@
+function Queen() {
+
+
+    var tasks = {
+        stone:0,
+
+    }
+
+  this.ImHere = function(Ant) {
+
+    
+  };
+}
