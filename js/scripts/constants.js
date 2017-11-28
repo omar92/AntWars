0bc221d86646a25796c05a46988600d354a0750a
AUEzzat <amr.m.ezzat@gmail.com>
2017-11-28 12:10:53 +0200

    dank

--- js/scripts/constants.js
+++ js/scripts/constants.js
@@ -1,13 +1,14 @@
-var RESOURCES ={
-    STONE:0,
-    WATER:1,
-    WOOD:2,
-    FOOD:3
+var RESOURCES = {
+    STONE: 0,
+    WATER: 1,
+    WOOD: 2,
+    FOOD: 3
 };
-var RESOURCES_LOCATIONS ={
-    QUEEN:0,
-    LAKE:1,
-    WOOD:2,
-    FOOD:3
+var LOCATIONS = {
+    QUEEN: 0,
+    LAKE: 1,
+    WOOD: 2,
+    FOOD: 3,
+    STONE: 4,
+    STORE: 5
 };
-
