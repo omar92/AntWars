1625c85493fe1a3d87e9766e231f9b00bec4bd2a
AUEzzat <amr.m.ezzat@gmail.com>
2017-11-28 11:13:46 +0200

    init

---
+++ main.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(1200, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+//load assets
+function preload() {
+
+}
+
+//create objects
+function create() {
+
+}
+
+//update and render
+function update() {
+
+}
