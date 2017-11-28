e519922f53b6bc1d793cd1d77c8db03d775e584a
ENG_OMARSLEAM\omar <omarabdelzaher@gmail.com>
2017-11-28 12:43:40 +0200

    queen update

--- js/scripts/queen.js
+++ js/scripts/queen.js
@@ -1,13 +1,43 @@
 function Queen() {
-
-
-    var tasks = {
-        stone:0,
-
+  var nextTaskIndex = 0;
+  var tasksPeriorities = [];
+  //Private ------------------------------------------------------------------------------------------------------
+  function assignTask(ant) {
+    for (let task = 0; task < tasksPeriorities.length; task++) {
+      var taskPeriority = tasksPeriorities[task];
     }
+  }
 
-  this.ImHere = function(Ant) {
+  function countTasksType(taskLocation) {
+    var num = 0;
+    for (let i = 0; i < tasksPeriorities.length; i++) {
+      if (tasksPeriorities[i] == taskLocation) {
+        num++;
+      }
+    }
+    return num;
+  }
 
-    
+  function removeTask(taskLocation) {
+    var index = 0;
+    for (let i = 0; i < tasksPeriorities.length; i++) {
+      if (tasksPeriorities[i] == taskLocation) {
+        tasksPeriorities.splice(i, 1);
+        return true; //remove Done
+      }
+    }
+    return false; //rmove failed
+  }
+  //Global ------------------------------------------------------------------------------------------------------
+  this.increasePeriority = function(taskLocation) {
+    if (countTasksType(taskLocation) < 4) {
+      tasksPeriorities.push(taskLocation);
+    }
   };
+  this.decreasePeriority = function(resource) {
+    if (countTasksType(taskLocation) > 0) {
+      removeTask(taskLocation);
+    }
+  };
+  this.ImHere = function(Ant) {};
 }
